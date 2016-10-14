eb7a3ea3492bd7f6d91d89eec3dda76bfb639b20
Filcer <gabfiltzer@gmail.com>
2016-10-13 23:25:48 -0500

    Falta ElimNodo
    Checar cual de los dos métodos de busq y busqrec funciona mejor

--- src/Arbol.cpp
+++ src/Arbol.cpp
@@ -5,28 +5,56 @@ Arbol::Arbol(int orig)
 	this->Raiz = new NodoA(orig);
 }
 
-bool Arbol::AddNodo(int ref, int aux)
-{
-	if(this->Raiz == NULL)
-	{
-		this->Raiz = new NodoA(aux);
-		std::cout << "El arbol esta vacio, no existe la referencia" << std::endl;
-		return false;
-	}
-	NodoA* Padre = BusqNodo(ref);
-	if(Padre == NULL)
-	{
-		std::cout << "No existe la referencia" << std::endl;
-		return false;
-	}
-	else
-	{
-		NodoA* hijo = new NodoA(aux, Padre);
-		Padre->Hijos.push_back(hijo);
-	}
-	return true;
+Nodo* Arbol::Buscar(int ref){ 
+	if(raiz==NULL) //no se sí raiz está bien escrita como nodo
+		return NULL;
+	return BusqRec(ref,Raiz)
 }
 
+Nodo* Arbol::BusqRec(int ref,Nodo* aux){
+	Nodo* busqueda=aux;
+	Nodo* Res;
+	if(busqueda->getDato()==ref) //creo que getDato no está definida en nodo
+		return busqueda;
+	if(ref<busqueda->getDato() && Hizq != NULL) //checar sintaxis de Hizq
+		res=BusqRec(ref,Hizq); // ¿se puede llamar a busqref dentro de busqref?
+	else if(ref>busqueda->getDato() && Hder!= NULL) //sintaxis de Hder
+		res=BusqRec(ref,Hder);
+}
+
+
+bool Arbol::AddRec(int dato, Nodo* aux)
+{
+	bool res;
+	if(dato<aux.dato){
+	    if(Hizq!=NULL) //aquí no sé como escribir el hijo izquierdo
+		    res=AddRec(dato,Hizq); //aquí tampoco sé como
+	    else{
+	    	Nodo new= nodo(dato,aux); 
+		aux.Hizq=nodo; //aquí tampoco
+		return true;
+	    }
+	}
+	else if(dato>aux.dato){
+		if(HDer!=NULL) //aquí no sé como escribir el hijo derecho
+			res=AddRec(dato,HDer) //aquí tampoco sé como
+		else{
+		   Nodo new=nodo(dato,aux);
+		   aux.Hder=nodo; //aquí tampoco
+		   return true;
+		}
+		return false;
+	}	
+}
+
+bool Arbol::AddNodo(int dato)
+{
+	if(Buscar(dato)!=NULL)
+		return true;
+	return AddRec(dato,Raiz); //no se si la raiz esta bien escrita		
+}
+
+/*
 bool Arbol::ElimNodo(int ref)
 {
 	if (this->Raiz == NULL)
@@ -110,3 +138,4 @@ int Arbol::ImprimirRecursivo(NodoA* nodo)
 		
 	}
 }
+*/
