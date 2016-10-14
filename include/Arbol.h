8d61e02be6ea342cde5f82cd454c86e18aa11682
Filcer <gabfiltzer@gmail.com>
2016-10-13 22:41:45 -0500

    Add files via upload

---
+++ include/Arbol.h
@@ -0,0 +1,27 @@
+/* Nombres: Gabriel Filtzer Gorbach
+	    Daniel Ponce de León Súchil
+   
+   Descripción: En esta clase se encuentran los métodos necesarios para buscar un elemento en el arbol, y para agregar y 
+   eliminar un nodo deseado. Además también está definida la búsqueda recursiva (BusqRec), la cuál simplifica el proceso 
+   de busqueda de un dato. 
+	    */
+
+#include <stack>
+#include "NodoA.h"
+
+class Arbol
+{
+  private:
+    NodoA* BusqRec(int ref, NodoA* aux);
+    int ImprimirRecursivo(NodoA* nodo);
+
+  public:
+    NodoA* Raiz;
+    Arbol(int orig);
+    NodoA* BusqNodo(int ref);
+    bool AddNodo(int ref, int aux);
+    bool ElimNodo(int ref);
+    std::stack<NodoA*> Camino(NodoA* aux);
+    void Imprimir();
+
+};
---
+++ include/NodoA.h
@@ -0,0 +1,22 @@
+/* Nombres: Gabriel Filtzer Gorbach
+	    Daniel Ponce de León Súchil
+   
+   Descripción: En esta clase se encuentran lo métodos que determinan la creación 
+   de un nodo y sus apuntadores correspondientes.
+   
+	    */
+
+#include <iostream>
+#include <vector>
+
+class NodoA
+{
+
+public:
+	int dato;
+	std::vector<NodoA*> Hijos; 
+	NodoA* Padre;
+	NodoA(int dato);
+	NodoA(int dato , NodoA* padre);
+  
+};
